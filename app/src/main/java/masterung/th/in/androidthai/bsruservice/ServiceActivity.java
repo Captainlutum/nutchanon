0de2f83677ec72a2fb91ab368c62e508a380cfe8
nutchanon <tumkubeiei@gmail.com>
2019-03-05 13:38:02 +0700

    13.37 new

--- app/src/main/java/masterung/th/in/androidthai/bsruservice/ServiceActivity.java
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/ServiceActivity.java
@@ -1,13 +1,24 @@
 package masterung.th.in.androidthai.bsruservice;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
-    }
-}
+
+
+
+
+    } // Main Method
+} // Main Class
